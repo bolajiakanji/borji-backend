01ed5c51c7c77238e44755bcbc70351d7dbdac23
Lukman Bolaji <bolajiakanji21@gmail.com>
2025-07-14 09:14:27 -0700

    connect to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/borjiNew', { ignoreUndefined: true})
+mongoose.connect('mongodb+srv://bolajiakanji21:dsoQ40Z0wLpz5UTq@cluster0.uwhsgsd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('connected to mongoDB...'))
   .catch(err => console.error('Could not connect to mongoDB...', err)) 
