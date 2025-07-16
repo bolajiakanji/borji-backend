1b70de0de1ffdc610c439fd66f817c2c1a777b14
Lukman Bolaji <bolajiakanji21@gmail.com>
2025-07-15 23:51:57 -0700

    fix listening port issues

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const app = express();
+
 
 mongoose.connect('mongodb+srv://bolajiakanji21:dsoQ40Z0wLpz5UTq@cluster0.uwhsgsd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('connected to mongoDB...'))
   .catch(err => console.error('Could not connect to mongoDB...', err)) 
 
 
-//   const port = process.env.PORT || config.get("port");
-  const port = 3000
+  //const port = process.env.PORT || config.get("port");
+  const port = process.env.PORT || 3000;
+  
 app.listen(port, function() {
   console.log(`Server started on port ${port}...`);
 });
