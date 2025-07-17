e4e08e0b1cf01d0096652b0a0561a0e2f4de88a8
Lukman Bolaji <bolajiakanji21@gmail.com>
2025-07-17 09:53:38 -0700

    Add thrd party middleware

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 const config = require("config");
+const cors = require('cors');
+require('dotenv').config()
+const helmet = require("helmet");
+const compression = require("compression");
 
 
 
 mongoose.connect('mongodb+srv://bolajiakanji21:dsoQ40Z0wLpz5UTq@cluster0.uwhsgsd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('connected to mongoDB...'))
   .catch(err => console.error('Could not connect to mongoDB...', err)) 
+
+app.use(cors())
+app.use(express.static("public"));
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
+
 
 
   //const port = process.env.PORT || config.get("port");
