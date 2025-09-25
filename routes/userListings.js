35f8e6090a2112075cf7003c2ebb11cf038b8dd4
Lukman Bolaji <bolajiakanji21@gmail.com>
2025-09-25 08:48:35 -0700

    create user listing route for get and delete request

--- routes/userListings.js
+++ routes/userListings.js
@@ -5,9 +5,9 @@ const Listings = require("../models/listings");
 
 router.get("/:id",auth,  async (req, res) => {
   console.log('lets go')
-  const userId = req.params.id;
+  const _id = req.params.id;
  
-    const userListings = await Listings.find({ userId })
+    const userListings = await Listings.find({ _id })
 
   if (userListings.length < 1) return res.status(404).send([])
     console.log('userListings21')
@@ -15,6 +15,14 @@ router.get("/:id",auth,  async (req, res) => {
   console.log(userId)
   console.log('you say waiting')
   res.status(200).send(userListings);
+});
+
+router.delete("/:id",auth,  async (req, res) => {
+  console.log('lets go')
+  const _id = req.params.id;
+ 
+    const userListings = await Listings.deleteOne({ _id })
+res.status(200).send(userListings);
 });
 
 module.exports = router;
